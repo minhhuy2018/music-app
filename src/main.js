d4f28f7bea2e39a2510279e20c92e3276891d45d
huangmingyu <780403303@qq.com>
2019-09-18 20:33:09 +0800

    首页添加内容

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Heardernav from "@/components/Heardernav.vue";
+
+Vue.use(ElementUI);
+
+Vue.component("header-nav",Heardernav);
 
 Vue.config.productionTip = false
 
--- src/router.js
+++ src/router.js
@@ -10,12 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
     },
+
+
     {
       path: '/about',
-      name: 'about',
+    
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
